7a2eddb349b9bea7589724a910733e20df2bc959
Noxsum <100073830+Noxsum@users.noreply.github.com>
2023-04-04 04:26:22 +0200

    pagina crear oferta

---
+++ paginas-bootstrap/crear_oferta/assets/js/add_req.js
@@ -0,0 +1,10 @@
+function add_req() {
+  var dropdown = document.getElementById("req_drop");
+  var label = document.getElementById("req_text");
+  var selectedOption = dropdown.options[dropdown.selectedIndex].text;
+  if (label.innerHTML.indexOf(selectedOption) === -1) {
+      label.innerHTML += "       " + selectedOption;
+  } else {
+      alert("Elemento ya añadido")
+  }
+}
