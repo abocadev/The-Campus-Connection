99180a40063b6053f0e7046834b5b611d6a128bf
abocadev <albert.bocanegra2003@gmail.com>
2023-04-20 18:00:51 +0200

    udate

--- execute.php
+++ execute.php
@@ -10,4 +10,5 @@ echo "".shell_exec("php bin/console doctrine:database:create");
 echo "".shell_exec("php bin/console doctrine:migrations:diff");
 echo "".shell_exec("php bin/console doctrine:migrations:migrate");
 echo "".shell_exec("php bin/console doctrine:fixtures:load");
+echo "".shell_exec("php bin/console cache:clear");
 echo "".shell_exec("php -S localhost:8080 -t public/");
